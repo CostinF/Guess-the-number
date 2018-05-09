c1a3828cd81b94db80c352f64f1322152ae5c131
Costin <costin.faraian@gmail.com>
2018-05-09 11:10:42 +0300

    Programul gata.

--- guess the number/Program.cs
+++ guess the number/Program.cs
@@ -10,6 +10,27 @@ namespace guess_the_number
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            int r = rnd.Next(0, 100);
+            Console.WriteLine("Guess the number. Introduceti un numar intre 0 si 100: ");
+            string str = Console.ReadLine();
+            int x = int.Parse(str);
+            while (x != r)
+            {
+                while (x > r)
+                {
+                    Console.WriteLine("Numarul " + x + " este prea mare. Introduceti alt numar: ");
+                    str = Console.ReadLine();
+                    x = int.Parse(str);
+                }
+                while (x < r)
+                {
+                    Console.WriteLine("Numarul " + x + " este prea mic. Introduceti alt numar: ");
+                    str = Console.ReadLine();
+                    x = int.Parse(str);
+                }
+            }
+            Console.WriteLine("Ai ghicit numarul. Felicitari!");
         }
     }
 }
